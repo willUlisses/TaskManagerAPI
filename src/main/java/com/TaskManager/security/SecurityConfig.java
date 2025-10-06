47b1747eb3a5960ca3d1251960e9b86cccf71e2c
willUlisses <williamcavalcante06@gmail.com>
2025-10-06 01:31:29 -0300

    Added: JWT oncePerRequestFilter and fully implemented TokenService class

--- src/main/java/com/TaskManager/security/SecurityConfig.java
+++ src/main/java/com/TaskManager/security/SecurityConfig.java
@@ -1,10 +1,14 @@
 package com.TaskManager.security;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
+@Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
-    // adicionar meu securityFilterChain e meu filter before
 
+    // importar meu securityFilter pro addFilterBefore
+    // adicionar meu securityFilterChain e meu filter before
+    // Criar um bean de um encoder bcrypt
 }
--- src/main/java/com/TaskManager/security/SecurityFilter.java
+++ src/main/java/com/TaskManager/security/SecurityFilter.java
@@ -1,7 +1,54 @@
 package com.TaskManager.security;
 
-public class SecurityFilter {
+import com.TaskManager.models.User;
+import com.TaskManager.repositories.UserRepository;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+public class SecurityFilter extends OncePerRequestFilter {
+
+    private final TokenService tokenService;
+    private final UserRepository userRepository;
+
+    public SecurityFilter(TokenService tokenService, UserRepository userRepository) {
+        this.tokenService = tokenService;
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response
+            , FilterChain filterChain) throws ServletException, IOException {
+
+        String token = this.recoverToken(request);
+
+        if (token != null) {
+            String username = tokenService.validateToken(token);
+            UserDetails user = userRepository.findByUsername(username);
+            if (user != null) {
+                var usernamePasswordAuth = new UsernamePasswordAuthenticationToken(
+                        user,
+                        null,
+                        user.getAuthorities()
+                );
+                SecurityContextHolder.getContext().setAuthentication(usernamePasswordAuth);
+            }
+        }
+        filterChain.doFilter(request, response);
+    }
 
 
+    private String recoverToken(HttpServletRequest request) {
+        String token = request.getHeader("Authorization");
+        if (token == null) return null;
 
+        return token.replace("Bearer ", "");
+    }
 }
