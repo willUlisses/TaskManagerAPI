c4d42e803ec23d8a3c7a680cbd5cd5e610624fb6
willUlisses <williamcavalcante06@gmail.com>
2025-10-08 01:33:23 -0300

    FIX: SecurityConfig requestMatchers endpoints (need to fix them again)

--- src/main/java/com/TaskManager/security/SecurityConfig.java
+++ src/main/java/com/TaskManager/security/SecurityConfig.java
@@ -1,15 +1,62 @@
 package com.TaskManager.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
+    private final SecurityFilter tokenFilter;
 
-    // importar meu securityFilter pro addFilterBefore
+    public SecurityConfig(SecurityFilter tokenFilter) {
+        this.tokenFilter = tokenFilter;
+    }
     // adicionar meu securityFilterChain e meu filter before
-    // Criar um bean de um encoder bcrypt
+
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf(AbstractHttpConfigurer::disable)
+                .sessionManagement(session ->
+                        session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(HttpMethod.GET, "/tasks/find").hasRole("ADMIN")
+                        .requestMatchers(HttpMethod.GET, "/tasks/find/{id}").hasRole("ADMIN")
+                        .requestMatchers(HttpMethod.GET, "/tasks/find/title").hasAnyRole("ADMIN", "USER")
+                        .requestMatchers(HttpMethod.POST, "/tasks").hasAnyRole("ADMIN, USER")
+                        .requestMatchers(HttpMethod.GET, "/categories/find").hasRole("ADMIN")
+                        .requestMatchers(HttpMethod.GET, "/categories/find/name").hasAnyRole("ADMIN", "USER")
+                        .requestMatchers(HttpMethod.POST, "/categories").hasAnyRole("ADMIN", "USER")
+                        .requestMatchers(HttpMethod.DELETE, "/categories/delete/{id}").hasRole("ADMIN")
+                        .requestMatchers(HttpMethod.GET, "/users/find").hasRole("ADMIN")
+                        .requestMatchers(HttpMethod.GET, "/users/find/{id}").hasRole("ADMIN")
+                        .requestMatchers(HttpMethod.POST, "/users").hasRole("ADMIN")
+                        .requestMatchers(HttpMethod.DELETE, "/users/delete/{id}").hasRole("ADMIN")
+                        .anyRequest().authenticated())
+                .addFilterBefore(tokenFilter, UsernamePasswordAuthenticationFilter.class)
+                .build();
+    }
+
+
+    public AuthenticationManager authManager(AuthenticationConfiguration authConfiguration)
+            throws Exception {
+        return authConfiguration.getAuthenticationManager();
+    }
+
+
+    @Bean
+    public BCryptPasswordEncoder encoder() {
+        return new BCryptPasswordEncoder();
+    }
 
 }
